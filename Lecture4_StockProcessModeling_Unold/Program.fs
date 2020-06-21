6e2f1b118db9c7d92d403a95d1199fb62171e98f
Kuba Unold <jakub@unold.pl>
2020-06-21 12:08:44 +0200

    Add project files.

---
+++ Lecture4_StockProcessModeling_Unold/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let getRandom = System.Random().Next()%2 |> System.Convert.ToBoolean
+
+let simulateRandomWalk (n: int) =
+    //printfn "%i" n
+    let r = getRandom
+    
+    r
+
+[<EntryPoint>]
+let main argv =
+    //printfn "Hello World from F#!"
+    //simulateRandomWalk 7
+    
+
+    
+    
+    0 // return an integer exit code
